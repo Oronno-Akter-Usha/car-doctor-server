32a4a866153dcc30a4ef5970c48a1c61613dde9a
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-05-09 00:36:08 +0600

    added json web token

---
+++ jwt-steps.js
@@ -0,0 +1,26 @@
+/**
+ *
+ *  install jsonwebtoken
+ *  jwt.sign(payload, secret, {expiresIn :})
+ *  send token to cilent side
+ *
+ **/
+
+/**
+ *
+ * how to store token in the client side
+ *  1. memory --> ok type {not recommended}
+ *  2. local storage  --> ok type (XSS)
+ *  3. cookies: http only
+ *
+ **/
+
+/**
+ *
+ *  1. set cookies with http only. for deplopment secure: false,
+ *
+ *  2. cors
+ *
+ *  3. client side axios setting
+ *
+ **/
